282ef224cbed82ca816dd3ae384a001d2ec653ee
Nikolay Gniteev <godhart@gmail.com>
2021-11-29 19:53:30 +0300

    + scripts for running anvil.works server

---
+++ running/migrate_schems_fun.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+anvil-app-server --app schematic_capture --auto-migrate --config-file schems_fun.anvil-config.yaml
---
+++ running/run_schems_fun.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+anvil-app-server --app schematic_capture --config-file schems_fun.anvil-config.yaml
